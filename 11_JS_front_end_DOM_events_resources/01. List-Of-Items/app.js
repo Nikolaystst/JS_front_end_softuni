695445266e7927f258ce673fe8030cb406a0ccc8
Nikolaystst <148203113+Nikolaystst@users.noreply.github.com>
2024-03-31 10:53:38 +0300

    DOM ex

--- 11_JS_front_end_DOM_events_resources/01. List-Of-Items/app.js
+++
@@ -1,9 +0,0 @@
-function addItem() {
-    let getItemsElement = document.getElementById('items');
-    let newElement = document.createElement('li');
-    let userInputElement = document.getElementById('newItemText');
-
-    newElement.textContent = userInputElement.value;
-    getItemsElement.appendChild(newElement);
-    userInputElement.value = '';
-}
